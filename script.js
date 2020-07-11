ee19412d0b29e3c85f94443d5036f80f01262c2f
kati <fanomezantsoakatiuscia048@gmail.com>
2020-07-11 16:01:38 +0300

    changing some of the elements

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 // Add elements we need
 
-const newPost = document.querySelector('#form-card'); 
-const newPostTitle = document.querySelector('.new-title');
-newPostTitle.setAttribute("postTitle", " " );
-const newPostContent = document.querySelector('.card-text');
-newPostContent.setAttribute("postContent", " " );
-const newPostImg = document.querySelector('.card-img-top');
-newPostImg.width = 500 * 200;
-const newAuthor = document.querySelector('new-post-author');
-const newPostDate = document.querySelector('.card-footer text-muted');
-const submitBtn = document.querySelector('.btn btn-primary');
+const newPost = document.querySelector('.card'); 
+const newPostTitle = document.createElement("h5");
+newPostTitle.classList.add("new-post-title");
+const newPostContent = document.createElement('textarea');
+newPostContent.classList.add("new-post-text")
+const newAuthor = document.createElement('label');
+newAuthor.classList.add("new-author");
+const submitBtn = document.getElementsByClassName('btn btn-primary');
 
-//Listen if the user add word more than 20 
-newPostContent.addEventListener('input', (event) => {
-  // if length >= 20 ----> invalid-feedback
-  if (event.target.value.length > 20) {
-    // setAttribute invalid-feedback 
-    newPostContent.setAttribute("invalid", "true");
-  } 
-});
+const myImg = document.createElement('img');
+const imgWidth = 500 * 200;
+myImg.setAttribute('src', `https://picsum.photos/${imgWidth}`);
+myImg.width = 500 * 200;
 
 //Add eventListnesr to the submit button
-document.getElementsByClassName('btn btn-primary').addEventListener("click", (event) => {
-const myNewBlogPost = (newPostTitle, newPostContent, newAuthor, newPostDate) =>{
-    return `
-    <div class = "card-body">
-      <h5 class = "card-title>${newPostTitle.value}</h5>
-      <textarea class = "card-text">${newPostContent.value}</textarea>
-      <label class = "new-post-author"> by ${newAuthor}</label>
-      <div class = ".card-footer text-muted">${newPostDate}</div>
-      <img class = ".card-img-top">${newPostImg.value}</img>
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+const myNewBlogPost =`
+    <div class = "card">
+        <div class = "card-body">
+            <h5 class = "new-post-title">${newPostTitle.value}</h5>
+            <textarea class = "new-post-text">${newPostContent.value}</textarea>
+            <label class = "new-author"> by ${newAuthor.value}</label>
+            <img src= "" data-width="500 * 200"></img>
+        </div>
     </div>
     <button>Delete</button>
 `;
-};
 
-//Create delete button 
-const deleteBtn = document.createElement("button");
-deleteBtn.textContent = "Delete";
-newPost.innerHTML += myNewBlogPost;
+ 
+// Create delete button 
+ const deleteBtn = document.createElement("button");
+ deleteBtn.textContent = "Delete";
+ newPost.innerHTML += myNewBlogPost;
+});
+
+
+// Listen if the user add word more than 20 
+newPostContent.addEventListener('input', (event) => {
+    // if length >= 20 ----> invalid-feedback
+       if (event.target.value.length > 20) {
+         // setAttribute invalid-feedback 
+         newPostContent.setAttribute("invalid", "true");
+} 
 });
